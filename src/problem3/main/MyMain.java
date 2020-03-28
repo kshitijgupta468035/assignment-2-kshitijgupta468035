29d59db293c37675823cd77c90132c6d66a2d215
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 14:49:42 +0530

    create a object of MyPriorityQueue<Student> name myPriorityQueue<Student>

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,16 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue<Student> myPriorityQueue = new MyPriorityQueue<>();
+
+
+    }
 }
