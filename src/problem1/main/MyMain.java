f97e790d85f33bf51263f540fdf0aa4047fb5bca
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 12:24:55 +0530

    add vlaue through call method add

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -12,6 +12,17 @@ import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree<Integer> tree = new MyBinarySearchTree<>();
-
+        tree.add(100);
+        tree.add(50);
+        tree.add(200);
+        tree.add(25);
+        tree.add(75);
+        tree.add(150);
+        tree.add(300);
+        tree.add(40);
+        tree.add(60);
+        tree.add(110);
+        tree.add(250);
+        tree.add(275);
     }
 }
