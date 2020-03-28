6be81832d97536457f79df1887c0789ac26bcd14
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 11:19:26 +0530

    make three private variables

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,10 +5,18 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
+    private TreeNode<E> root;
+    private int numberOfLeftChild = 0;
+    private int maxLevel = 0;
+
     @Override
     public boolean add(E data) {
         return false;
     }
+
 }
