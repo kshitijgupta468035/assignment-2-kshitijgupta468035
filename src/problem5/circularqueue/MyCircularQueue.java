3184f4fd7f382d20bd85619efc55dd11571d2e92
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 16:06:51 +0530

    MyCircularQueue implements CircularQueue

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,33 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.adt.CircularQueue;
+
 //to implement circular queue
-public class MyCircularQueue<E> {
+public class MyCircularQueue<E> implements CircularQueue<E> {
+    @Override
+    public void enqueue(E data) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
 }
