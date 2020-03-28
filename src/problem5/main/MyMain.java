b74fb977bc0eba9de8e7bbccb760fc6882b5141f
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 16:13:41 +0530

    add a for loop for the user to enter the details

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -16,5 +16,22 @@ public class MyMain {
     public static void main(String[] args) {
         MyCircularQueue<Student> myCircularQueue = new MyCircularQueue<>();
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of Students you want to add in queue :");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter the detail of Student" + i + " :");
+            System.out.print("Enter name :");
+            String name = scanner.nextLine();
+            System.out.print("Enter roll Number :");
+            int rollNumber = scanner.nextInt();
+            System.out.print("Enter number of BackLogs :");
+            int backLog = scanner.nextInt();
+            scanner.nextLine();
+            Student st = new Student(rollNumber, name, backLog);
+            myCircularQueue.enqueue(st);
+        }
+
     }
 }
