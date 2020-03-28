3d23867c7a267d9140e68adcdb53dfebe38807ce
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 16:04:51 +0530

    add getter and setter

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -17,5 +17,27 @@ public class Student {
         this.backLog = backLog;
     }
 
+    public int getRollNumber() {
+        return rollNumber;
+    }
 
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBackLog() {
+        return backLog;
+    }
+
+    public void setBackLog(int backLog) {
+        this.backLog = backLog;
+    }
 }
