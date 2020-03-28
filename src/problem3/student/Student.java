d9dc8cf211ed0cd30ab7ba795e69d78cafd30405
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 13:42:52 +0530

    add constructor of Student class

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -4,4 +4,9 @@ public class Student {
     private int rollNumber;
     private String name;
 
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
 }
