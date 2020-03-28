e729f7e9bf1e5b6b289fe7a85927ff8345c691d6
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 12:22:36 +0530

    create a object name tree of class MyBinarySearchTree<Integer>

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,11 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> tree = new MyBinarySearchTree<>();
+
+    }
 }
