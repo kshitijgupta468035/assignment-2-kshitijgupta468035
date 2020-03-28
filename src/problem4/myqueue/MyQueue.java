d6d68108e945b59ce300bba7f95aea7b2dad831d
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 15:42:59 +0530

    add method String toString

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -53,6 +53,19 @@ public class MyQueue<E> implements MyQueueADT<E> {
         return size;
     }
 
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("[");
+        Node<E> temp = front;
+        for (int i = 0; i < size && temp != null; i++) {
+            E data = temp.getData();
+            sb.append(data);
+            sb.append((i < size - 1) ? "," : "");
+            temp = temp.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
     private static class Node<E> {
         E data;
         Node<E> next;
