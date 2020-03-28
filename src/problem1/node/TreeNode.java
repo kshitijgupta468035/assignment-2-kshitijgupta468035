431aa08b4637959fc69dae5f50eacdc921108a71
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-28 10:44:17 +0530

    create getter and setter method for all private instance variable of the class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -14,4 +14,28 @@ public class TreeNode<E> {
     public TreeNode(E data) {
         this.data = data;
     }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public TreeNode<E> getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode<E> leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode<E> getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode<E> rightChild) {
+        this.rightChild = rightChild;
+    }
 }
